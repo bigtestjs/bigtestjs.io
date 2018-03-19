b68df3a13f0026ba963452aeb6f64e174e54c135
Wil Wilsman <wil@frontside.io>
2018-03-19 14:25:13 -0500

    Generate API docs from package

--- config.rb
+++ config.rb
@@ -12,15 +12,6 @@ set :markdown,
 # Layouts
 page '/docs/*', layout: 'docs'
 
-# Proxy pages
-
-# Helpers
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
 # Dev-specific config
 configure :development do
   activate :livereload
@@ -30,4 +21,33 @@ end
 configure :build do
   activate :minify_css
   activate :minify_javascript
+end
+
+# API Docs
+Dir.glob('./node_modules/@bigtest/*').each do |path|
+  pkg = JSON.parse(File.read("#{path}/package.json"), object_class: OpenStruct)
+  docs = JSON.parse(File.read("#{path}/docs/data.json"), object_class: OpenStruct)
+
+  url_path = pkg.name.gsub(%r{@bigtest/(.*)}, '/docs/\1.html')
+  proxy url_path, 'api-doc.html', locals: { pkg: pkg, docs: docs }, ignore: true
+end
+
+# Kramdown parser for span level tags only
+class Kramdown::Parser::Spandown < Kramdown::Parser::GFM
+  def initialize(source, options)
+    super
+    @block_parsers = []
+  end
+end
+
+# Helpers
+helpers do
+  def markdown(text)
+    Kramdown::Document.new(text, config[:markdown]).to_html if text
+  end
+
+  def markdown_inline(text)
+    md_conf = config[:markdown].merge(input: 'Spandown')
+    Kramdown::Document.new(text, md_conf).to_html if text
+  end
 end
