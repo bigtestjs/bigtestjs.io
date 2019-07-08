fe3ae740cd9f2425f628a6a4931abbd89c75a586
Jeffrey Cherewaty <jeffrey@frontside.io>
2019-07-08 15:39:45 -0500

    Migrate to Gatsby

--- source/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-import SidebarComponent from './sidebar';
-import TabbedLayoutComponent from './tabbed-layout';
-
-const { hljs } = window;
-
-hljs.initHighlighting();
-SidebarComponent.init('.sidebar');
-TabbedLayoutComponent.init('.tabbed-layout');
--- source/javascripts/utils.js
+++
@@ -1,7 +0,0 @@
-export function $$(selector, ctx = document.body) {
-  return [].slice.call(ctx.querySelectorAll(selector));
-}
-
-export function isVisible($el) {
-  return !!$el.getClientRects().length;
-}
