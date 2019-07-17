1e38a6f5d3130e96b34cc2064503f8a369882c8c
Jeffrey Cherewaty <jeffrey@frontside.io>
2019-07-17 16:33:56 -0500

    Got colors working

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -27,17 +27,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <Footer></Footer>
-      </div>
+        <main className="main">{children}</main>
+      <Footer></Footer>
     </>
   )
 }
